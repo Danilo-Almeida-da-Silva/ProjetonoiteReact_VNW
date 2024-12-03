f47706e55ddd3be9a4d4f0755364eda1767d9599
Danilo Almeida <danilo.laet52@gmail.com>
2024-12-03 19:59:15 -0300

    Atualização  da estrutura da page Livros doados

--- src/Pages/livrosDoados/LivrosDoados.jsx
+++ src/Pages/livrosDoados/LivrosDoados.jsx
@@ -1,9 +1,22 @@
-
+import livrodoado from '../../assets/livrodoado.png'
+import s from '../../Pages/livrosDoados/livro.module.scss'
 
 export default function LivrosDoados(){
     return(
-     <main>
+        <section className={s.livroiniciodoados}>
+            <h2>Livros Doados</h2>
        
-     </main> 
+            <section className={s.sectionlivro}>
+                <section className={s.amostradolivro}>
+                    <img src={livrodoado} alt="Icone de um livro fechado com capa vermelha para representar o livro doado" />
+                    <div> 
+                        <p>O protagonista</p>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+            </section>
+        </section>
+     
     )
 }
