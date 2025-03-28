f88e62bff1ce29b2981e9be0ebc81c24edd21ccb
Danilo Almeida <danilo.laet52@gmail.com>
2025-03-28 20:19:47 -0300

    feat: adiciona axios e novas funções de requisição para conectar a api ao Backend

--- src/Pages/queroDoar/QueroDoar.jsx
+++ src/Pages/queroDoar/QueroDoar.jsx
@@ -1,25 +1,90 @@
+import axios from 'axios'
 import livroforms from '../../assets/Livroforms.png'
 import s from '../../Pages/queroDoar/QueroDoar.module.scss'
+import { useState } from 'react'
 
-export default function Querodoar(){
-    return(
-        <section className={s.sectiondoar}>
+export default function Querodoar() {
+  const [titulo, setTitulo] = useState('')
+  const [categoria, setCategoria] = useState('')
+  const [autor, setAutor] = useState('')
+  const [image_url, setImage_url] = useState('')
 
-            <p>Por favor, preencha o formulário com suas informações e as informações do Livro!</p>
+  const capturaTitulo = (e) => {
+    setTitulo(e.target.value)
+  }
 
-            <form action="">
-            
-                <div>
-                    <img src={livroforms} alt="Icone de um livro aberto com borda azul" />
-                    <h2>Informações do Livro</h2>
-                </div>
+  const capturaCategoria = (e) => {
+    setCategoria(e.target.value)
+  }
 
-                <input type="text" name="" id="" placeholder='Titulo' />
-                <input type="text" name="" id="" placeholder='Categoria' />
-                <input type="text" name="" id="" placeholder='Autor' />
-                <input type="text" name="" id="" placeholder='Link da Imagem' />
-                <input type="submit" value="Doar" className={s.buttonDoar}/>
-            </form>
-        </section>
+  const capturaAutor = (e) => {
+    setAutor(e.target.value)
+  }
+
+  const capturaImage_url = (e) => {
+    setImage_url(e.target.value)
+  }
+
+  const envioDados = async () => {
+    const enviarParadados = {
+      titulo,
+      categoria,
+      autor,
+      image_url,
+    }
+    await axios.post(
+      'https://api-flask-python-w3my.onrender.com/doar',
+      enviarParadados
     )
+  }
+
+  return (
+    <section className={s.sectiondoar}>
+      <p>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro!
+      </p>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <img src={livroforms} alt='Icone de um livro aberto com borda azul' />
+          <h2>Informações do Livro</h2>
+        </div>
+
+        <input
+          type='text'
+          name=''
+          id=''
+          placeholder='Titulo'
+          onChange={capturaTitulo}
+        />
+        <input
+          type='text'
+          name=''
+          id=''
+          placeholder='Categoria'
+          onChange={capturaCategoria}
+        />
+        <input
+          type='text'
+          name=''
+          id=''
+          placeholder='Autor'
+          onChange={capturaAutor}
+        />
+        <input
+          type='text'
+          name=''
+          id=''
+          placeholder='Link da Imagem'
+          onChange={capturaImage_url}
+        />
+        <input
+          type='submit'
+          value='Doar'
+          className={s.buttonDoar}
+          onClick={envioDados}
+        />
+      </form>
+    </section>
+  )
 }
